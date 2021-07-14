9ca26f55bb72ffcccf75e1a2dccf8b2fb83d2f0f
Er <gauchecolibri@gmail.com>
2021-07-14 16:10:39 -0400

    Add files via upload

---
+++ VMTvsHGF Regression Example.R
@@ -0,0 +1,68 @@
+#FDOT VMT DVMT vs HFG EDA and Regression Analysis
+
+# VMT=Vehicle Miles Traveled
+# DVMT= Daily Vehicle Miles Traveled
+# HGF= Highway Miles of Fuel consumed
+
+dataset=read.csv('VMTvsHGF2.csv')
+
+#Creating Scatter Plot between VMT (x) and HGF (y)
+
+library(ggplot2)
+scatter<-ggplot(dataset, aes(TVMT, THGF))
+scatter+geom_point()+labs(x="Vehicle Miles Traveled", y="Highway Gallons of Fuel")+
+  geom_smooth()
+
+# `geom_smooth()` using method = 'loess' and formula 'y ~ x'
+# Loess regression is a nonparametric technique that uses local weighted 
+# regression to fit a smooth curve through points in a scatter plot. 
+# Loess curves are can reveal trends and cycles in data that might be difficult
+# to model with a parametric curve. COnfidence interval is at 95%
+
+# Changing Method to create a "straight line" with 95% confidence interval
+scatter+geom_point()+labs(x="Million Vehicle Miles Traveled", y="Million Highway Gallons of Fuel")+
+  geom_smooth(method="lm", colour="Cyan")
+
+#Scatter plot without confidence interval
+scatter+geom_point()+labs(x="Vehicle Miles Traveled", y="Highway Gallons of Fuel")+
+  geom_smooth(method="lm", colour="Red", se=F)
+
+#Robust linear model used. Need to add MASS package:
+library(MASS)
+scatter+geom_point()+labs(x="Vehicle Miles Traveled", y="Highway Gallons of Fuel")+
+  geom_smooth(method="rlm", colour="Orange", se=F)
+
+# Robust regression is an alternative to least squares regression when data are 
+# contaminated with outliers or influential observations, and it can also be used
+# for the purpose of detecting influential observations.
+
+#Robust regression with 99% confidence interval
+scatter+geom_point()+labs(x="Vehicle Miles Traveled", y="Highway Gallons of Fuel")+
+  geom_smooth(method="rlm", colour="Blue", level=0.99, alpha=0.1, fill="Cyan")
+
+#Robust regression with 99% confidence interval
+scatter2<-ggplot(dataset, aes(DVMT, HGF))
+scatter2+geom_point()+labs(x="Daily Vehicle Miles Traveled", y="Highway Gallons of Fuel")+
+  geom_smooth(method="rlm", colour="Blue", level=0.99, alpha=0.1, fill="Cyan")
+
+# Fitting linear Regression for VMT vs HGF
+
+regressor<-lm(formula=THGF~ TVMT, data<-dataset) 
+
+#Summary table
+summary(regressor)
+
+#Plot residuals
+
+par(mfrow = c(2, 2))  # Split the plotting panel into a 2 x 2 grid
+plot(regressor) 
+par(mfrow = c(1, 1))
+
+
+#Residuals Analysis Plots: Extended version.
+# library(lindia)
+# gg_diagnose(regressor)
+# gg_resX(regressor)
+# library(olsrr)
+# ols_test_normality(regressor)
+# ols_plot_resid_hist(regressor)
